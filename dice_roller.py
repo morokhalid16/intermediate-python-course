f284b9f8407fbf2e7559aef2e8ee0f79286b7d0a
root <root@DESKTOP-TNEVL9R.localdomain>
2022-02-10 00:47:21 +0100

    adding for loop over rollers

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,13 @@
+
 import random 
 def main():
-  roll=random.randint(1,6)
-  print(f'you roll a {roll}')
+  dice_rolls=2
+  dice_sum=0
+  for i in range(dice_rolls):
+	  
+   roll=random.randint(1,6)
+   dice_sum+=1
+   print(f'you roll a {roll} ')
+  print(f' you have rolled {dice_sum} ')
 if __name__== "__main__":
   main()
